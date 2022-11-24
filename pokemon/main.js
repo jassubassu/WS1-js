8c3cd3c238b7d00f023d4ee049ef8779b6069dbb
jassubassu <jasmin.sainio@gmail.com>
2022-11-24 17:31:10 +0200

    luotiin lista halutusta datasta

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,62 +1,75 @@
-window.addEventListener('load'), async () => {
-    /* nimen hakeminen /
-    const nameInput = document.querySelector('#name');
-    const username = localStorage.getItem('username') || '';
-    nameInput.value = username;
-    / nimen tallennus /
-    nameInput.addEventListener('change', e => {
-        localStorage.setItem('username', e.target.value);
-    });
-
+window.addEventListener('load', async () => {
     const pokemonData = await haePokemonData();
+    const pokemonsWithMoreData = await getMoreData(pokemonData);
 
-    displayTodos(pokemonData);
+    displayPokemons(pokemonsWithMoreData);
 });
+
+/* haetaan data pokemon apista ja palautetaan lista pokemoneja */
 
 async function haePokemonData() {
     // lisää url tähän
-    const pokemonUrl = "";
+    const pokemonUrl = "https://pokeapi.co/api/v2/pokemon";
 
     try {
         // tee fetch kutsu
-        const pokemonPromise = "";
+        const pokemonPromise = await fetch(pokemonUrl);
         // muuta jsoniksi
-        const pokemonData = ""
+        const pokemonData = await pokemonPromise.json();
 
         // palauta lista pokemoneja
-        return {};
+        return pokemonData.results;
     } catch (error) {
         console.log(error);
         return [];
     }
 };
 
-function displayTodos(items) {
+async function getMoreData(pokemonData) {
+
+    const fillInformation = await Promise.allSettled(pokemonData.map(async pokemon => {
+        const pokemonUrl = pokemon.url;
+
+        const pokemonPromise = await fetch(pokemonUrl);
+        // muuta jsoniksi, pokemonData on tämä https://pokeapi.co/api/v2/pokemon/23/
+        const pokemonData = await pokemonPromise.json();
+
+        const elementType = pokemonData.types.map(type => type.type.name);
+
+        const imageLink = pokemonData.sprites.front_default;
+
+        return {
+            image:imageLink, name:pokemon.name, type:elementType
+        };
+    }))
+    return fillInformation.map(data => data.value);
+}
+
+function displayPokemons(pokemonData) {
     // hae divi jonka sisään pokemonit tulee
-    const pokeList = document.querySelector();
+    const pokeList = document.querySelector("#pokemon-result");
 
     pokeList.innerHTML = '';
 
-    items.forEach(pokemon => {
-        / 
-        yksi pokemon näyttää esim tältä
-        {
-            name: "bulbasaur"
-        }
-        /
+    console.log(pokemonData);
+
+    pokemonData.forEach(pokemon => {
+    
         const pokemonItem = document.createElement('div');
         pokemonItem.classList.add('todo-item');
 
         const content = document.createElement('div');
         content.classList.add('todo-content');
 
-        content.innerHTML = `<div>
-            <h4>${{/tähän pokemonin nimi*/}}</h4>
-        </div>`;
+        content.innerHTML =
+        `<h4>${pokemon.name}</h4> <img src=${pokemon.image}> <p>${pokemon.type}</p>`;
 
         // lisää content pokemonitemin childiksi
 
+        pokemonItem.appendChild(content);
+        
         // lisää pokemonItem pokeListin childiksi
 
+        pokeList.appendChild(pokemonItem);
     })
 };
