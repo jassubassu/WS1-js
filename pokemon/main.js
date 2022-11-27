65ab5683133fd808f2ead61d3cf7ef462cf44626
jassubassu <jasmin.sainio@gmail.com>
2022-11-27 21:22:30 +0200

    näytetään yksi pokemon kerralla

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,79 +1,59 @@
 window.addEventListener('load', async () => {
-    const pokemonData = await haePokemonData();
-    const pokemonsWithMoreData = await getMoreData(pokemonData);
+    const searchTerm = document.querySelector('#search');
 
-    displayPokemons(pokemonsWithMoreData);
+    if (!searchTerm.value) {
+        await searchPokemon("pikachu");
+    }
+    addEventListener('submit', async (event) => {
+        event.preventDefault();
+        await searchPokemon(searchTerm.value);
+    });
 });
 
-/* haetaan data pokemon apista ja palautetaan lista pokemoneja */
-
-async function haePokemonData() {
-    // lisää url tähän
-    const pokemonUrl = "https://pokeapi.co/api/v2/pokemon";
-
+async function searchPokemon(hakutermi) {
+    const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${hakutermi}`;
     try {
         // tee fetch kutsu
         const pokemonPromise = await fetch(pokemonUrl);
         // muuta jsoniksi
         const pokemonData = await pokemonPromise.json();
+        console.log(pokemonData);
 
-        // palauta lista pokemoneja
-        return pokemonData.results;
+        const elementType = pokemonData.types.map(type => type.type.name);
+        const imageLink = pokemonData.sprites.other.dream_world.front_default;
+        const abilities = pokemonData.abilities.map(abilities => abilities.ability.name);
+
+        // palauta tietyt kentät pokemonista
+        const data = {
+            image:imageLink, name:pokemonData.name, type:elementType, abilities:abilities
+        };
+        
+        displayPokemons(data);
     } catch (error) {
         console.log(error);
         return [];
     }
-};
-
-async function getMoreData(pokemonData) {
-
-    const fillInformation = await Promise.allSettled(pokemonData.map(async pokemon => {
-        const pokemonUrl = pokemon.url;
-
-        const pokemonPromise = await fetch(pokemonUrl);
-        // muuta jsoniksi, pokemonData on tämä https://pokeapi.co/api/v2/pokemon/23/
-        const pokemonData = await pokemonPromise.json();
-
-        const elementType = pokemonData.types.map(type => type.type.name);
-
-        const imageLink = pokemonData.sprites.other.dream_world.front_default;
-
-        const abilities = pokemonData.abilities.map(abilities => abilities.ability.name);
-
-        console.log(abilities);
-        return {
-            image:imageLink, name:pokemon.name, type:elementType, abilities:abilities
-        };
-    }))
-    return fillInformation.map(data => data.value);
 }
 
-function displayPokemons(pokemonData) {
+function displayPokemons(data) {
     // hae divi jonka sisään pokemonit tulee
     const pokeList = document.querySelector("#pokemon-result");
 
     pokeList.innerHTML = '';
 
-    console.log(pokemonData);
+    const pokemonItem = document.createElement('div');
+    const content = document.createElement('div');
 
-    pokemonData.forEach(pokemon => {
+    content.innerHTML = `
+        <h4 id="pokemonNimi">${data.name}</h4> 
+        <img id="kuva" src=${data.image}>
+        <p id="pokemonTyyppi">Type: <br>${data.type}</p> 
+        <p id="pokemonAbilities">Abilities: <br>${data.abilities}</p>
+    `;
+
+    // lisää content pokemonitemin childiksi
+    pokemonItem.appendChild(content);
     
-        const pokemonItem = document.createElement('div');
-        pokemonItem.classList.add('todo-item');
-
-        const content = document.createElement('div');
-        content.classList.add('todo-content');
-
-        content.innerHTML =
-        `<h4 id="pokemonNimi">${pokemon.name}</h4> <img id="kuva" src=${pokemon.image}>
-        <p id="pokemonTyyppi">Type: <br>${pokemon.type}</p> <p id="pokemonAbilities">Abilities: <br>${pokemon.abilities}</p>`;
-
-        // lisää content pokemonitemin childiksi
-
-        pokemonItem.appendChild(content);
-        
-        // lisää pokemonItem pokeListin childiksi
-
-        pokeList.appendChild(pokemonItem);
-    })
+    // lisää pokemonItem pokeListin childiksi
+    pokeList.appendChild(pokemonItem);
 };
